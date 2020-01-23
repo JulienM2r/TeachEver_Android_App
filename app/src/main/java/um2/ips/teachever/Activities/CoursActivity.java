51123ca2616b8ec3b62fdd2f5f515a3326be885b
JulienM2r <julien.morgan-de-rivery@etu.umontpellier.fr>
2020-01-23 08:20:08 +0100

    final Up

---
+++ app/src/main/java/um2/ips/teachever/Activities/CoursActivity.java
@@ -0,0 +1,12 @@
+package um2.ips.teachever.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoursActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
